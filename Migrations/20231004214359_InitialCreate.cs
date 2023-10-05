90686d992b627a83d4696180a2393d8b8cf76f9d
Sean Dorris <seanc.dorris@gmail.com>
2023-10-05 09:54:13 -0500

    first migration attempt successful

---
+++ Migrations/20231004214359_InitialCreate.cs
@@ -0,0 +1,239 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace HHPW_Serverside.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "itemTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_itemTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "orderStatuses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    statusName = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_orderStatuses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "orderTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    orderTypeName = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_orderTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "paymentTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    paymentTypeDesc = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_paymentTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    uid = table.Column<string>(type: "text", nullable: false),
+                    isEmployee = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "items",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    itemTypeId = table.Column<int>(type: "integer", nullable: false),
+                    itemName = table.Column<string>(type: "text", nullable: false),
+                    price = table.Column<decimal>(type: "numeric", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_items", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_items_itemTypes_itemTypeId",
+                        column: x => x.itemTypeId,
+                        principalTable: "itemTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "orders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    orderStatusId = table.Column<int>(type: "integer", nullable: false),
+                    orderTypeId = table.Column<int>(type: "integer", nullable: false),
+                    paymentTypeId = table.Column<int>(type: "integer", nullable: false),
+                    uid = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_orders", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_orders_orderStatuses_orderStatusId",
+                        column: x => x.orderStatusId,
+                        principalTable: "orderStatuses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_orders_orderTypes_orderTypeId",
+                        column: x => x.orderTypeId,
+                        principalTable: "orderTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_orders_paymentTypes_paymentTypeId",
+                        column: x => x.paymentTypeId,
+                        principalTable: "paymentTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ItemOrder",
+                columns: table => new
+                {
+                    OrdersId = table.Column<int>(type: "integer", nullable: false),
+                    itemsId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ItemOrder", x => new { x.OrdersId, x.itemsId });
+                    table.ForeignKey(
+                        name: "FK_ItemOrder_items_itemsId",
+                        column: x => x.itemsId,
+                        principalTable: "items",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ItemOrder_orders_OrdersId",
+                        column: x => x.OrdersId,
+                        principalTable: "orders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "reviews",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    content = table.Column<string>(type: "text", nullable: false),
+                    orderId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_reviews", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_reviews_orders_orderId",
+                        column: x => x.orderId,
+                        principalTable: "orders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItemOrder_itemsId",
+                table: "ItemOrder",
+                column: "itemsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_items_itemTypeId",
+                table: "items",
+                column: "itemTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_orders_orderStatusId",
+                table: "orders",
+                column: "orderStatusId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_orders_orderTypeId",
+                table: "orders",
+                column: "orderTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_orders_paymentTypeId",
+                table: "orders",
+                column: "paymentTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_reviews_orderId",
+                table: "reviews",
+                column: "orderId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ItemOrder");
+
+            migrationBuilder.DropTable(
+                name: "reviews");
+
+            migrationBuilder.DropTable(
+                name: "users");
+
+            migrationBuilder.DropTable(
+                name: "items");
+
+            migrationBuilder.DropTable(
+                name: "orders");
+
+            migrationBuilder.DropTable(
+                name: "itemTypes");
+
+            migrationBuilder.DropTable(
+                name: "orderStatuses");
+
+            migrationBuilder.DropTable(
+                name: "orderTypes");
+
+            migrationBuilder.DropTable(
+                name: "paymentTypes");
+        }
+    }
+}
